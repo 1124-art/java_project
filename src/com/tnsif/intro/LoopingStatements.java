2e4d31ca801ec4bad56ce18a2ecec0463a4bb1ea
chikky_pathuri <pathurichikkyreddy@gmail.com>
2025-12-23 20:46:38 +0530

    loopingstatements

---
+++ src/com/tnsif/intro/LoopingStatements.java
@@ -0,0 +1,37 @@
+package com.tnsif.intro;
+class LoopingStatements{
+	
+		public static  void evenNumbers() {
+		//for loop
+		//to print even numbers
+		for(int i=2;i<10;i+=2) {
+			System.out.println("evenNumbers are:"+ i);
+		}
+		 
+	}
+		public static void oddNumbers() {
+			//while loop
+			int j=1;
+			while(j<10) {
+				j+=2;
+				System.out.println("odd numbers are:"+j);
+			}
+		}
+		public static void Numbers() {
+		
+		        int i = 1;
+
+		        do {
+		            System.out.println(i);
+		            i++;
+		        } while (i <= 5);
+		    }
+		
+
+		public static void main(String[]args) {
+			evenNumbers();
+			oddNumbers();
+			Numbers();
+	}
+
+}
